558cba1cf76fc9401a29b03e1ff03f0854a49dd2
MgZawHeinHtet <zawheinhtet1223@gmail.com>
2024-05-05 18:34:11 +0630

    finish detail page with responsive

---
+++ src/components/courses/Lesson.jsx
@@ -0,0 +1,25 @@
+import { clock } from "../../assets/icons";
+
+export default function Lesson({ lesson, i }) {
+  function calcDurationFromMinute(duration) {
+    return lesson.duration >= 60
+      ? lesson.duration === 60
+        ? `${lesson.duration / 60}hour`
+        : `${lesson.duration / 60}hour ${lesson.duration % 60} minutes`
+      : `${lesson.duration} minutes`;
+  }
+  return (
+    <li className="flex justify-between tablet:items-center tablet:gap-1 gap-7 tablet:flex-row flex-col p-4  shadow rounded-lg border-0 border-orange-6 hover:ring hover:ring-orange-4 hover:border hover:border-orange-6 transition-all duration-500">
+      <div className="space-y-1 ">
+        <h6 className="text-grey-7">{lesson.name}</h6>
+        <p className="text-grey-4">Lesson 0{i + 1}</p>
+      </div>
+      <div className="">
+        <div className="tablet:block clear-both inline-block bg-white-5 py-2 laptop:px-3 tablet:px-6 px-4 rounded-lg space-x-2 hover:bg-orange-4 ">
+          <img className=" float-left" src={clock} alt="clock" />
+          <span className="text-grey-4">{calcDurationFromMinute(lesson.duration)}</span>
+        </div>
+      </div>
+    </li>
+  );
+}
--- src/pages/courses/CourseDetail.page.jsx
+++ src/pages/courses/CourseDetail.page.jsx
@@ -1,8 +1,7 @@
 import { useParams } from "react-router";
 import { courses } from "../../components/courses/course-list";
 import { Container } from "../../components/layouts";
-import { Hero } from "../../components/courses";
-import { courseDetailContainer } from "../../assets/images/courses";
+import { Hero,CurriculumList, CurriculumCard } from "../../components/courses";
 import { clock } from "../../assets/icons";
 
 export default function CourseDetail() {
@@ -12,31 +11,11 @@ export default function CourseDetail() {
   return (
     <Container>
       <Hero title={course.title} content={course.content} />
-      <div className="py-20">
-        <img src={courseDetailContainer} alt="cover img" />
-      </div>
-      {/* curri list  */}
-      <div className="grid tablet:grid-cols-2 grid-cols-1 gap-5">
-        {/* single  curri */}
-        <div className="bg-white-1 p-10 shadow rounded-lg space-y-8">
-          <h6 className="text-grey-7 text-5xl font-bold text-right">01</h6>
-          <h5 className="font-semibold text-grey-5 text-lg capitalize">Introduction to UI/UX design</h5>
-          <ul className="p-5">
-            <li className="flex justify-between items-center tablet:flex-row flex-col">
-                <div>
-                    <h6>Understanding uI/Ux design</h6>
-                    <p>Lesson 01</p>
-                </div>
-                <div className="flex">
-                    
-                       <img src={clock} alt="" /> <span>45 minute</span>
-                    
-                </div>
-            </li>
-          </ul>
-        </div>
-      </div>
+      <CurriculumList >
+      {course.curriculums.map((curr, i) => (
+         <CurriculumCard key={i} curr={curr} i={i}/>
+        ))}
+      </CurriculumList>
     </Container>
-    
   );
 }
