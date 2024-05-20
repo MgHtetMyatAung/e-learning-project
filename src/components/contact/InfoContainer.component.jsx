98fe52684f10f6efb6e123de62273a73e8fc60fd
MgZawHeinHtet <zawheinhtet1223@gmail.com>
2024-05-20 11:06:47 +0630

    change icon size and padding and align of text

--- src/components/contact/InfoContainer.component.jsx
+++ src/components/contact/InfoContainer.component.jsx
@@ -5,7 +5,7 @@ import { contactList } from "../../data/ContactList";
 
 export default function InfoContainer() {
   return (
-    <div className="desktop:space-y-[50px] laptop:space-y-[24px] space-y-[20px] laptop:col-span-1 col-span-3 desktop:p-[80px] laptop:p-[60px] p-[30px]">
+    <div className="desktop:space-y-[50px] laptop:space-y-[24px] space-y-[20px] laptop:col-span-1 col-span-3 desktop:p-[80px] laptop:p-[40px] p-[30px]">
       {/* card  */}
       {contactList.map((contact, index) => (
         <IconAndText key={index} icons={contact.icons} label={contact.label} />
